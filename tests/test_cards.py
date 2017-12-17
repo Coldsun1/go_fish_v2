e8c26fb0f9ebff953c7f44fb4b35c1044e333e2c
Cold Sun <bikingrox@gmail.com>
2017-12-17 17:08:28 -0500

    Derp! Derp!

--- tests/test_cards.py
+++ tests/test_cards.py
@@ -24,6 +24,20 @@ def test_sort():
     for i in extras.types:
         player2.hand.append(i)
     random.shuffle(player2.hand)
-    
+
     player2.sort()
     assert player2.hand == extras.types
+
+def test_search_sets():
+    player3 = Hand()
+
+    for i in range(4):
+        player3.hand.append('A')
+        player3.hand.append('Q')
+
+    assert len(player3.hand) == 8
+
+    player3.search_sets()
+
+    assert len(player3.hand) == 0
+    assert len(player3.sets) == 2
