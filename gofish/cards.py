f347a53f171172e7583c914afcb06e53dcffa2c0
Cold Sun <bikingrox@gmail.com>
2017-12-17 17:16:18 -0500

    temp

--- gofish/cards.py
+++ gofish/cards.py
@@ -34,7 +34,9 @@ class Hand(object):
         for i in ex.types:
             if self.hand.count(i) >= 4:
                 for _ in range(4):
-                    print('What?')
                     self.hand.remove(i)
 
                 self.sets.append(i)
+
+    def which(self):
+        print("1")
