8a4b3b2638a6686e62155ee948ef951dc536fa30
Cold Sun <bikingrox@gmail.com>
2017-12-19 16:59:49 -0500

    updating

--- tests/__init__.py
+++
@@ -1,5 +0,0 @@
-import gofish.ask as ask
-import gofish.cards as cards
-import gofish.display as display
-import gofish.extras as extras
-import gofish.player as player
--- tests/test_ask.py
+++ tests/test_ask.py
@@ -1,25 +1,25 @@
 import pytest
-import tests.__init__ as ts
+from gofish import ask, cards, extras
 
 
 def test_is_player():
-    player1 = ts.cards.Hand('player1')
-    player2 = ts.cards.Hand('player2')
+    player1 = cards.Hand('player1')
+    player2 = cards.Hand('player2')
     player3 = None
 
     players = [player1, player2]
 
-    assert ts.ask.is_player(player1, players)
-    assert not ts.ask.is_player(player3, players)
+    assert ask.is_player(player1, players)
+    assert not ask.is_player(player3, players)
 
 def test_is_card():
-    assert ts.ask.is_card('A', ts.extras.types)
+    assert ask.is_card('A', extras.types)
 
 def test_clean_word():
 
-    assert ts.ask.clean_word('WORD') == 'word'
-    assert ts.ask.clean_word('WORD ONLY !!! DERP _+@!*(%)') == 'wordonlyderp'
+    assert ask.clean_word('WORD') == 'word'
+    assert ask.clean_word('WORD ONLY !!! DERP _+@!*(%)') == 'wordonlyderp'
 
 def test_clean_sentence():
-    assert ts.ask.clean_sentence('THIS IS A SENTENCE!') == ['this', 'is', 'a', 'sentence']
-    assert ts.ask.clean_sentence('123 $$$$$ play```er1') == ['123', 'player1']
+    assert ask.clean_sentence('THIS IS A SENTENCE!') == ['this', 'is', 'a', 'sentence']
+    assert ask.clean_sentence('123 $$$$$ play```er1') == ['123', 'player1']
